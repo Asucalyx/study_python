aa480e77cbd9776ea3e2e7486ff65cf9641267f9
20202103419 <1520470365@qq.com>
2024-01-13 20:33:36 +0800

    第四天
    主要以做题为主

---
+++ python假期学习/lianxi.py
@@ -0,0 +1,17 @@
+def shared_chars_count(word1, word2):
+    # 此处编写代码
+    str1 = word1.lower().replace(" ","")
+    str2 = word2.lower().replace(" ","")
+    str1 = sorted(str1)
+    str2 = sorted(str2)
+    for i in range()
+
+# 获取输入
+word1 = input()
+word2 = input()
+
+# 调用函数
+print(shared_chars_count(word1, word2))
+
+
+
