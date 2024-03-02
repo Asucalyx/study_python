81423aa33eeb815a3d498f5c4cd769738c5bcb31
20202103419 <1520470365@qq.com>
2024-03-02 09:07:57 +0800

    1

--- python_study/6_文件操作/2_文件的写操作.py
+++ python_study/6_文件操作/2_文件的写操作.py
@@ -1,2 +1,9 @@
-#文件写入write()方法 直接调用将会写入缓冲区而非真正写入文件
-#内容刷新
+#文件写入write()方法 直接调用将会写入缓冲区而非真正写入文件,避免频繁操作硬盘
+f = open("D:/GitHub/study_python/python_study/7_测试文件夹/2_测试","w",encoding="UTF-8")
+f.write("hello asuka!!!")
+
+#内容刷新flush()方法 将内存中积攒的内容写入硬盘的文件中
+f.flush()
+
+#close()方法其实内置了flush()方法
+f.close()
---
+++ python_study/6_文件操作/3_文件的追加操作.py
@@ -0,0 +1,4 @@
+f = open("D:/GitHub/study_python/python_study/7_测试文件夹/3_测试","a",encoding="UTF-8")
+f.write("hello asuka!")
+
+f.close()
